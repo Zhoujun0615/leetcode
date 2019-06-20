297327d6f24dbbae2c7e2bdb1a2e4906947b87ee
Zhoujun <zhoujunhqu@163.com>
2019-06-20 16:55:52 +0800

    test

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author:zhoujun
+ * @date:2019/6/20 16:51
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
